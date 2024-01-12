84676a0c2899dbc5bf6619cd2b96400beed4c7a5
Darius <dariusvalere@gmail.com>
2024-01-11 19:05:22 -0500

    Variables and button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { getTokenFromUrl } from './spotify';
 
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const REDIRECT_URI = "https://localhost:3001"
+const CLIENT_ID = "0d1f0eb1410845f5a0ffdc1639baba7f"
+const RESPONSE_TYPE = "token"
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
       </header>
     </div>
   );
