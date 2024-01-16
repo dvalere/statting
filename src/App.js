44b2145a868959f20fd995261966492262f01bcf
Darius <dariusvalere@gmail.com>
2024-01-16 10:40:20 -0500

    Fixing location of variables, adding more functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-const REDIRECT_URI = "https://localhost:3001"
-const CLIENT_ID = "0d1f0eb1410845f5a0ffdc1639baba7f"
-const RESPONSE_TYPE = "token"
 
 function App() {
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const REDIRECT_URI = "https://localhost:3000"
+  const CLIENT_ID = "0d1f0eb1410845f5a0ffdc1639baba7f"
+  const RESPONSE_TYPE = "token"
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+}, [])
+
+const logout = () => {
+  setToken("")
+  window.localStorage.removeItem("token")
+}
+
   return (
     <div className="App">
       <header className="App-header">
